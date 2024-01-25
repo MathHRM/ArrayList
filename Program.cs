1506c856fa565bd94c8c22d261064916ec78dd05
Matheus Resende <matheushrm614@gmail.com>
2024-01-25 13:34:53 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,19 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        ArrayList<string> teste = new();
-        teste.Push("palavra1");
-        teste.Push("a");
-        teste.Push("b");
-
-        teste.Print();
-
-        teste[1] = "naolavra";
-
-        teste.Print();
-
-        foreach(string palavra in teste){
-            Console.Write(palavra);
-        }
+        
     }
 }
